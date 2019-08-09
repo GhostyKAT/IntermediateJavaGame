9e24db065dd363ae97d780acf0a3287ef61d9661
ghostykat <ghostykatyt@gmail.com>
2019-08-09 19:30:07 -0400

    Organized dialogue into 'DialogueEngine'.

--- src/com/company/util/DialogueEngine.java
+++ src/com/company/util/DialogueEngine.java
@@ -1,5 +1,67 @@
 package com.company.util;
 
-public class DialogueEngine {
+import java.io.BufferedReader;
 
+public class DialogueEngine {
+    public static String yOrN(String prompt, BufferedReader reader) {
+        String result;
+        String anwser = "";
+        try {
+            do {
+                System.out.print(prompt);
+                System.out.flush();
+                anwser = reader.readLine();
+
+                System.out.print("Are You Sure? (Y/N) ");
+                result = reader.readLine();
+
+                result = result.toUpperCase();
+                while (!result.equals("Y") && !result.equals("N")) {
+                    System.out.println("That is not allowed.");
+                    System.out.print("Are You Sure? (Y/N) ");
+                    result = reader.readLine();
+                    result = result.toUpperCase();
+                }
+            } while (!result.equals("Y"));
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+        return anwser;
+    }
+
+    public static int getNumInRange(String prompt, BufferedReader reader, int min, int max) {
+        Integer numResult = null;
+        String yOrNResult;
+        try {
+            while(true) {
+                System.out.print(prompt);
+                System.out.flush();
+                numResult = Integer.parseInt(reader.readLine());
+                if(numResult >= min && numResult <= max) {
+
+                    System.out.print("Are You Sure? (Y/N) ");
+                    yOrNResult = reader.readLine().toUpperCase();
+
+                    while (!yOrNResult.equals("Y") && !yOrNResult.equals("N")) {
+                        System.out.println("That is not allowed.");
+                        System.out.print("Are You Sure? (Y/N) ");
+                        yOrNResult = reader.readLine();
+                        yOrNResult = yOrNResult.toUpperCase();
+
+                    }
+                    if(yOrNResult.equals("N")) {
+                        numResult = null;
+                    }
+                }
+                if(numResult != null) {
+                    if(numResult >= min && numResult <= max) {
+                        break;
+                    }
+                }
+            }
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+        return numResult;
+    }
 }
